2e7b8845de9a2ef13c1959d0e7bd588843e48d59
superbbird <superbbirds@gmail.com>
2015-11-25 16:40:56 +0330

    requestActivity : create request object init data & save move editText text to hint  storing hint in string.xml
     parseConstant:
     create parseConstant class  for using classes & data Constant

---
+++ app/src/main/java/com/example/pooria/bestoonchi/Parse/parseConstant.java
@@ -0,0 +1,22 @@
+package com.example.pooria.bestoonchi.Parse;
+
+import android.app.Application;
+
+/**
+ * Created by mm on 11/25/2015.
+ */
+public class parseConstant extends Application{
+    public static String request_Class_Name = "darkhast";
+    public static String request_Field_Name = "name";
+    public static String request_Field_Codekharidar = "codekharidar";
+    public static String request_Field_CodeDarkhast= "codedarkhast";
+    public static String request_Field_Senf = "senf";
+    public static String request_Field_Shahr = "shahr";
+    public static String request_Field_Tozihat = "tozihat";
+    public static String request_Field_Picture = "xae";
+    public static String request_Field_ExpireTime = "extime";
+
+
+
+}
+
--- app/src/main/java/com/example/pooria/bestoonchi/RequestActivity.java
+++ app/src/main/java/com/example/pooria/bestoonchi/RequestActivity.java
@@ -3,25 +3,57 @@ package com.example.pooria.bestoonchi;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.style.TtsSpan;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.pooria.bestoonchi.Parse.parseConstant;
+import com.parse.ParseObject;
+
+import java.util.Date;
 
 public class RequestActivity extends AppCompatActivity {
+    EditText name_editText;
+    EditText tozihat_editText;
+    EditText expireTime_editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.request);
 
-        Button btnsubmit=(Button)findViewById(R.id.button6);
+         name_editText  = (EditText) findViewById(R.id.request_name);
+        tozihat_editText  = (EditText) findViewById(R.id.request_tozihat);
+         expireTime_editText  = (EditText) findViewById(R.id.request_expireTime);
+
+        Button btnsubmit=(Button)findViewById(R.id.request_submit);
         btnsubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent =new Intent(RequestActivity.this,MainActivity.class);
-                startActivity(intent);
-                RequestActivity.this.finish();
+                //create object to access parse class
+                ParseObject requestObject = new ParseObject(parseConstant.request_Class_Name);
+
+                //put data in objcet
+                requestObject.put(parseConstant.request_Field_Name, "test name");
+                requestObject.put(parseConstant.request_Field_Tozihat, "test tozihat");
+
+                //save & send data to parse in backgroud (new thread )
+                requestObject.saveInBackground();
+
+                // back to previous activity
+                navigateToMain();
+
             }
         });
 
+    }
+
+    private void navigateToMain() {
+       // Intent intent =new Intent(RequestActivity.this,MainActivity.class);
+        //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        //startActivity(intent);
+        RequestActivity.this.finish();
     }
 }
