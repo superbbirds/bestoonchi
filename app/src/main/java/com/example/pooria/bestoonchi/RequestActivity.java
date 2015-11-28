63f92123f1564fdde057cc8bf382af68d33b70b2
superbbird <superbbirds@gmail.com>
2015-11-28 17:41:01 +0330

    add Image picker

--- app/src/main/java/com/example/pooria/bestoonchi/RequestActivity.java
+++ app/src/main/java/com/example/pooria/bestoonchi/RequestActivity.java
@@ -1,5 +1,13 @@
 package com.example.pooria.bestoonchi;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.os.Environment;
+import android.os.Parcelable;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,7 +18,10 @@ import android.widget.Toast;
 import com.parse.ParseObject;
 
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 public class RequestActivity extends AppCompatActivity {
     EditText name_editText;
@@ -31,6 +42,14 @@ public class RequestActivity extends AppCompatActivity {
                calendarParse = show_DatePicker();
             }
         });
+         findViewById(R.id.request_pick_image).setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 openImageIntent();
+                 ToastMe(outputFileUri.toString());
+
+             }
+         });
 
 
         Button btnsubmit=(Button)findViewById(R.id.request_submit);
@@ -114,4 +133,70 @@ public class RequestActivity extends AppCompatActivity {
     private void ToastMe(int message) {
         Toast.makeText(RequestActivity.this, message, Toast.LENGTH_LONG).show();
     }
+
+    private Uri outputFileUri;
+
+    private void openImageIntent() {
+
+// Determine Uri of camera image to save.
+        final File root = new File(Environment.getExternalStorageDirectory() + File.separator + "MyDir" + File.separator);
+        root.mkdirs();
+        final String fname ="img_"+ System.currentTimeMillis() + ".jpg";
+        final File sdImageMainDirectory = new File(root, fname);
+        outputFileUri = Uri.fromFile(sdImageMainDirectory);
+
+        // Camera.
+        final List<Intent> cameraIntents = new ArrayList<Intent>();
+        final Intent captureIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        final PackageManager packageManager = getPackageManager();
+        final List<ResolveInfo> listCam = packageManager.queryIntentActivities(captureIntent, 0);
+        for(ResolveInfo res : listCam) {
+            final String packageName = res.activityInfo.packageName;
+            final Intent intent = new Intent(captureIntent);
+            intent.setComponent(new ComponentName(res.activityInfo.packageName, res.activityInfo.name));
+            intent.setPackage(packageName);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+            cameraIntents.add(intent);
+        }
+
+        // Filesystem.
+        final Intent galleryIntent = new Intent();
+        galleryIntent.setType("image/*");
+        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+
+        // Chooser of filesystem options.
+        final Intent chooserIntent = Intent.createChooser(galleryIntent, "Select Source");
+
+        // Add the camera options.
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, cameraIntents.toArray(new Parcelable[cameraIntents.size()]));
+
+        startActivityForResult(chooserIntent, 999);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 999) {
+                final boolean isCamera;
+                if (data == null) {
+                    isCamera = true;
+                } else {
+                    final String action = data.getAction();
+                    if (action == null) {
+                        isCamera = false;
+                    } else {
+                        isCamera = action.equals(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    }
+                }
+
+                Uri selectedImageUri;
+                if (isCamera) {
+                    selectedImageUri = outputFileUri;
+                } else {
+                    selectedImageUri = data == null ? null : data.getData();
+                }
+            }
+        }
+    }
+
 }
